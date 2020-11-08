f148e41f5878ba835408df1f4d6d3756b57ec2cc
Miguel Cardenas <miguel.cardenas@grability.com>
2020-11-07 19:41:44 -0500

    add first config for backend

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import http from "http";
+import cors from "cors";
+import config from "./config";
+import resolvers from "./resolvers";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = require("socket.io")(server);
+
+// Use cors for all origins
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send({ message: "This is a Test" });
+});
+
+app.use(bodyParser.json());
+
+io.on("connection", socket => {
+  console.log("a user connected");
+});
+
+server.listen(config.PORT, () => {
+  console.log(
+    `Code_Sample server runing in port http://localhost:${config.PORT}`
+  );
+});
