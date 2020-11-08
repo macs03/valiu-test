b06b9a83c680baf5b5913ecd186c43d2fafdca96
Miguel Cardenas <miguelcarde.3@gmail.com>
2020-11-08 13:29:08 -0500

    using resolvers file

--- backend/src/resolvers.js
+++ backend/src/resolvers.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+  emitWelcomeMessage: socket => {
+    socket.broadcast.emit("hi");
+  },
+  disconect: socket => {
+    socket.on("disconnect", () => {
+      console.info("user disconnected", socket.client.id);
+    });
+  },
+  badgedEvent: (socket, io) => {
+    socket.on("badged", msg => {
+      console.info(msg);
+      io.emit("badged", msg);
+    });
+  }
+};
