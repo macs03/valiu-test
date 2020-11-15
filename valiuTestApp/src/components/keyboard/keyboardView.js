ac37bbf3d37532f8ef6356737b02bb3d666092d9
Miguel Cardenas <miguelcarde.3@gmail.com>
2020-11-14 20:28:20 -0500

    sending number to parent

--- valiuTestApp/src/components/keyboard/keyboardView.js
+++ valiuTestApp/src/components/keyboard/keyboardView.js
@@ -4,7 +4,7 @@ import {Text, TouchableOpacity, View} from 'react-native';
 
 import styles from './styles';
 
-const KeyboardView = ({title}) => {
+const KeyboardView = ({title, handlingPad}) => {
   const pad = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
 
   return (
@@ -12,7 +12,10 @@ const KeyboardView = ({title}) => {
       <Text style={styles.darkText}>{title}</Text>
       <View style={styles.pad}>
         {pad.map((item) => (
-          <TouchableOpacity style={styles.itemContainer}>
+          <TouchableOpacity
+            key={item}
+            onPress={() => handlingPad(item)}
+            style={styles.itemContainer}>
             <Text>{item}</Text>
           </TouchableOpacity>
         ))}
@@ -23,6 +26,7 @@ const KeyboardView = ({title}) => {
 
 KeyboardView.prototype = {
   title: PropTypes.string.isRequire,
+  handlingPad: PropTypes.func.isRequire,
 };
 
 export default KeyboardView;
--- valiuTestApp/src/screens/modal/modal.js
+++ valiuTestApp/src/screens/modal/modal.js
@@ -1,12 +1,14 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
 import PropTypes from 'prop-types';
+import React, {useState, useRef} from 'react';
+import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+
 import app from '../../lib/app';
 import styles from './styles';
 import KeyboardView from '../../components/keyboard/keyboardView';
 
 const Modal = ({navigation: {goBack}}) => {
   const [keyboardShowed, setKeyboardShowed] = useState(false);
+  const inputRef = useRef(null);
   let amount = 0;
 
   const sendMessage = () => {
@@ -19,6 +21,11 @@ const Modal = ({navigation: {goBack}}) => {
     setKeyboardShowed(flag);
   };
 
+  const onNumbers = (value) => {
+    console.log(value);
+    console.log(inputRef.current);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -28,14 +35,20 @@ const Modal = ({navigation: {goBack}}) => {
         </TouchableOpacity>
       </View>
       <View style={styles.inputContainer}>
-        <TextInput placeholder={'Add amount'} onFocus={showCustomKeyboard} />
+        <TextInput
+          placeholder={'Add amount'}
+          onFocus={showCustomKeyboard}
+          ref={inputRef}
+        />
         <TouchableOpacity
           style={styles.sendButton}
           onPress={() => sendMessage()}>
           <Text style={styles.lightText}>Send Amount</Text>
         </TouchableOpacity>
       </View>
-      {keyboardShowed && <KeyboardView title="Custom Keyboard" />}
+      {keyboardShowed && (
+        <KeyboardView title="Custom Keyboard" handlingPad={onNumbers} />
+      )}
     </View>
   );
 };
