2f5a15202ddb877ef8ee7738f8384e7778d54e8f
Miguel Cardenas <miguelcarde.3@gmail.com>
2020-11-14 22:16:30 -0500

    add splash test

---
+++ valiuTestApp/__tests__/screens/splash/splash.test.js
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import Splash from '../../../src/screens/splash/splash';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Splash />);
+  expect(tree).toMatchSnapshot();
+});
